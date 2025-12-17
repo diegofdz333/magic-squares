077b487e325f644fd995c2e38bab5c910529966e
Diego Fernandez <diegofdz2004@gmail.com>
2025-12-17 01:09:43 -0500

    Add comments.

--- src/MSParallel.hs
+++ src/MSParallel.hs
@@ -9,6 +9,7 @@ import Control.Parallel
 magicConstant :: Int -> Int
 magicConstant n = n * (n * n + 1) `div` 2
 
+-- Get all permutations of a specific length of a Set
 lengthNPermutations :: Set a -> Int -> [[a]]
 lengthNPermutations xs n = concatMap permutations $ subsequencesOfLength n $ toList xs
     where
@@ -17,6 +18,7 @@ lengthNPermutations xs n = concatMap permutations $ subsequencesOfLength n $ toL
         subsequencesOfLength _ []     = []
         subsequencesOfLength k (y:ys) = map (y:) (subsequencesOfLength (k-1) ys) ++ subsequencesOfLength k ys
 
+-- Check if a filled Square is Magic
 check :: Int -> Array (Int, Int) Int -> Bool
 check n square = checkRows 0 && checkCols 0 && checkDiagOne && checkDiagTwo && checkLastCornerLargest
     where
@@ -36,16 +38,20 @@ check n square = checkRows 0 && checkCols 0 && checkDiagOne && checkDiagTwo && c
 
         checkDiagTwo = sum [ square ! (n - i - 1, i) | i <- [0..(n-1)] ] == constant
 
-        -- For reflextion fixes
+        -- Reflection / Rotation Fix
         checkLastCornerLargest = square ! (0, 0) < square ! (n - 1, n - 1)
 
+-- Fill in a row of the magic square.
 row :: Int -> Int -> Array (Int, Int) Int -> Set Int -> Int
 row n pos square rest = possibleRows stepPerms
     where
         constant = magicConstant n
+        -- base the necesary sum of the permutation to make the row magic. Target constant minus elements already filled in.
         base = constant - sum [ square ! (pos, j) | j <- [0..(pos - 1)] ]
+        -- All permutations, ignores last element since that can be filled in automatically
         stepPerms = lengthNPermutations rest (n - 1 - pos)
-        possibleRows []     = 0
+
+        possibleRows []         = 0
         possibleRows (p:ps)
             | pos * 2 < targetD = pVal `par` possibleRows ps `pseq` (pVal + possibleRows ps)
             | otherwise         = pVal + possibleRows ps
@@ -53,24 +59,26 @@ row n pos square rest = possibleRows stepPerms
                 p0 = case p of
                     (p0':_)   -> p0'
                     []        -> error "tst"                
-                r = base - sum p
+                r = base - sum p -- The necessary value for the last element for row to be magic.
                 s = foldr delete rest p
                 pVal
-                    | pos == 0 && r < p0 = 0
+                    | pos == 0 && r < p0 = 0 -- Reflection / Rotation Fix
                     | member r s         = col n pos nextSquare $ delete r s
-                    | otherwise          = 0
+                    | otherwise          = 0 -- Prune if the necessary last element of row does not exist.
                 nextSquare = square // [((pos, j), v) | (j, v) <- zip [pos..] p]
                                     // [((pos, n - 1), r)]
 
+-- Fill in a column of the magic square.
 col :: Int -> Int -> Array (Int, Int) Int -> Set Int -> Int
 col n pos square rest
-    | S.null rest = if check n square then 1 else 0
+    | S.null rest = if check n square then 1 else 0 -- Done iterating, check if Magic
     | otherwise     = possibleCols stepPerms
     where
         constant = magicConstant n
         base = constant - sum [ square ! (i, pos) | i <- [0..pos] ]
         stepPerms = lengthNPermutations rest (n - 2 - pos)
-        possibleCols []     = 0
+
+        possibleCols []             = 0
         possibleCols (p:ps)
             | pos * 2 + 1 < targetD = pVal `par` possibleCols ps `pseq` (pVal + possibleCols ps)
             | otherwise             = pVal + possibleCols ps
@@ -79,7 +87,7 @@ col n pos square rest
                 r = base - sum p
                 s = foldr delete rest p
                 pVal
-                    | pos == 0 && r < s1 = 0
+                    | pos == 0 && r < s1 = 0 -- Reflection / Rotation Fix
                     | member r s                     = row n (pos + 1) nextSquare $ delete r s
                     | otherwise                      = 0
                 nextSquare = square // [((i, pos), v) | (i, v) <- zip [pos+1..] p]
--- src/MSParallelElements.hs
+++ src/MSParallelElements.hs
@@ -7,6 +7,7 @@ import Control.Parallel
 magicConstant :: Int -> Int
 magicConstant n = n * (n * n + 1) `div` 2
 
+-- Check if a filled Square is Magic
 check :: Int -> Array (Int, Int) Int -> Bool
 check n square = checkRows 0 && checkCols 0 && checkDiagOne && checkDiagTwo && checkLastCornerLargest
     where
@@ -26,35 +27,43 @@ check n square = checkRows 0 && checkCols 0 && checkDiagOne && checkDiagTwo && c
 
         checkDiagTwo = sum [ square ! (n - i - 1, i) | i <- [0..(n-1)] ] == constant
 
-        -- For reflextion fixes
+        -- Reflection / Rotation Fix
         checkLastCornerLargest = square ! (0, 0) < square ! (n - 1, n - 1)
 
+-- Iterate step by step on a row. 
+-- Pos is the index of the row in the square.
+-- Step is the index within the row we are filling in (Without counting cells filled in previously by colStep)
+-- Depth is used for parallelization
 rowStep :: Int -> Int -> Int -> Int -> Array (Int, Int) Int -> Set Int -> Int
 rowStep n pos step depth square rest = possibleRowSteps $ toList rest
     where
         possibleRowSteps [] = 0
         possibleRowSteps (p:ps)
+            -- Our rules for Rotations / Reflections means the first element cannot be these values, prune early to reduce time & GC
             | pos == 0 && step == 0 && (p > n * n - 3) = 0
-            | pos == n - 1     = lastRow
+            | pos == n - 1     = lastRow -- The last row has some special properties
             | depth < targetD  = pVal `par` possibleRowSteps ps `pseq` (pVal + possibleRowSteps ps)
             | otherwise        = pVal + possibleRowSteps ps
             where
                 pVal
-                    | step == n - 2 - pos = finalStep p
+                    | step == n - 2 - pos = finalStep p 
                     | otherwise           = rowStep n pos (step + 1) (depth + 1) nextSquare $ delete p rest
                 nextSquare = square // [((pos, step + pos), p)]
+        -- The final two elements of a row are done together
+        -- This is because the last element has to be constant minues the sum of the rest, so no more recursion is needed
         finalStep p = pVal
             where
                 constant = magicConstant n
                 base = constant - sum [ square ! (pos, j) | j <- [0..(n - 3)] ]
                 p0 = square ! (0, 0)
-                r = base - p
+                r = base - p -- The necessary value for the last element for row to be magic.
                 s = delete p rest
                 pVal
-                    | pos == 0 && r < p0 = 0
+                    | pos == 0 && r < p0 = 0 -- Reflection / Rotation Fix
                     | member r s         = colStep n pos 0 (depth + 1) nextSquare $ delete r s
-                    | otherwise          = 0
+                    | otherwise          = 0 -- Prune if the necessary last element of row does not exist.
                 nextSquare = square // [((pos, n - 2), p), ((pos, n - 1), r)]
+        -- The last row is actually just one element. Sum rest of row to figure its value then check if square is magic
         lastRow = pVal
             where
                 constant = magicConstant n
@@ -65,6 +74,10 @@ rowStep n pos step depth square rest = possibleRowSteps $ toList rest
                     where
                         newSquare  = square // [((pos, n - 1), r)]
 
+-- Iterate step by step on a column. 
+-- Pos is the index of the column in the square.
+-- Step is the index within the column we are filling in (Without counting cells filled in previously by rowStep)
+-- Depth is used for parallelization
 colStep :: Int -> Int -> Int -> Int -> Array (Int, Int) Int -> Set Int -> Int
 colStep n pos step depth square rest = possibleColSteps $ toList rest
     where
@@ -86,10 +99,11 @@ colStep n pos step depth square rest = possibleColSteps $ toList rest
                 r = base - p
                 s = delete p rest
                 pVal
-                    | pos == 0 && r < s1 = 0
+                    | pos == 0 && r < s1 = 0 -- Reflection / Rotation Fix
                     | member r s         = rowStep n (pos + 1) 0 (depth + 1) nextSquare $ delete r s
                     | otherwise          = 0
                 nextSquare = square // [((n - 2, pos), p), ((n - 1, pos), r)]
+        -- The last column is actually just one element. Sum rest of column to figure its value
         lastCol = pVal
             where
                 constant = magicConstant n
